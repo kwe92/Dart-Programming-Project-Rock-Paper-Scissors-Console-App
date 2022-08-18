fd24df8940f214b5d50d6414f4b7b61fd28ceb5f
Kweayon  Clark <Kweayon@gmail.com>
2022-08-18 16:18:30 -0500

    inital Commit

--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -22,25 +22,34 @@ void main() {
   var randNum;
   const gameText = 'Rock, Paper or Scissors? (r/p/s)';
   const List<String> gameChoices = ['r', 'p', 's'];
-  stdout.write(gameText);
-  final user_input = stdin.readLineSync().toString();
-  if (gameChoices.contains(user_input)) {
-    randNum = Random().nextInt(3);
-    aiMove = GameMoves.values[randNum];
-    userMove = GameMoves.values[gameChoices.indexOf(user_input)];
+  while (true) {
+    stdout.write(gameText);
+    final user_input = stdin.readLineSync().toString();
+    if (gameChoices.contains(user_input)) {
+      randNum = Random().nextInt(3);
+      aiMove = GameMoves.values[randNum];
+      userMove = GameMoves.values[gameChoices.indexOf(user_input)];
 
-    if (userMove == aiMove) {
-      print('''
-              AI selects: $aiMove
-              User selects: $userMove
-            ''');
+      if (userMove == aiMove) {
+        print('User selects: $userMove');
+        print('AI selects: $aiMove');
+        print('Resulting in a draw!');
+      } else if (userMove == GameMoves.Rock && aiMove == GameMoves.Scissors ||
+          userMove == GameMoves.Paper && aiMove == GameMoves.Rock ||
+          userMove == GameMoves.Scissors && aiMove == GameMoves.Paper) {
+        print('User selects: $userMove');
+        print('AI selects: $aiMove');
+        print('User wins!');
+      } else {
+        print('User selects: $userMove');
+        print('AI selects: $aiMove');
+        print('AI wins!');
+      }
+    } else if (user_input == 'q') {
+      break;
+    } else {
+      print(
+          'Incorrect input, use letters r, p, s to make a selection or q to quit the game.');
     }
-    print('AI plays: $aiMove');
-    print('User plays: $userMove');
-    // print(user_input);
-  } else {
-    print(
-        'Incorrect input, use (r/p/s) to make a selection or q to quit the game.');
   }
-  // print(user_input);
 }
